e5a3d533dd0ad11c55a18b339ec12b673040f6ed
alexleo-coder <leonchowdhury259@gmail.com>
2025-12-05 13:13:45 +0530

    some

---
+++ b.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize an integer array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Use a for loop to print each element
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
